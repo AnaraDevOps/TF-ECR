b062680e91160ec4408966e1f5ae7e1fcde62ad7
anara.a <ana.abdybekova@gmail.com>
2024-03-30 12:50:54 -0400

    changes to store ECR

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
-resource "aws_ecr_repository" "foo" {
-  name                 = "bar"
-  image_tag_mutability = "MUTABLE"
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-ecr-anarik"
+    dynamodb_table = "state_lock"
+    key            = "tf-ecr"
+    region         = "us-east-1"
+  }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = "my-ecr-repo"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
